525e9c4d3a26c105fd38ded8c4ea57b8d018f959
thomie <miedema@amolf.nl>
2010-05-19 14:28:57 +0200

    Change. Put everything in pipeline().

--- visualization/pipeline.py
+++ visualization/pipeline.py
@@ -12,7 +12,7 @@ RESOLUTION = 18
 from paraview.simple import *  # Should be listed first.
 #from paraview.servermanager import *
 
-def clear_pipeline():
+def clear():
     def name(proxy):
         return (type(proxy)).__name__
 
@@ -33,8 +33,6 @@ def clear_pipeline():
                 proxy.Input[0] = None
 
         Delete(proxy)
-
-clear_pipeline()
 
 
 '''
@@ -91,114 +89,117 @@ def add_tensor_glyph(data, type, name):
     rename(tensor_glyph, name)
     return tensor_glyph
 
+def pipeline():
+    clear()
+
+    if READERS:
+        # Read dynamic data.
+        files = addPVDReader(simulation_data_directory + '/files.pvd', 'DynamicDataReader')
+
+        # Read static data.
+        static = addPVDReader(simulation_data_directory + '/static.pvd', 'StaticDataReader')
 
 
-if READERS:
-    # Read dynamic data.
-    files = addPVDReader(simulation_data_directory + '/files.pvd', 'DynamicDataReader')
-
-    # Read static data.
-    static = addPVDReader(simulation_data_directory + '/static.pvd', 'StaticDataReader')
 
 
+    ### DYNAMIC
+
+    if PARTICLES:
+        # Particles.
+        particles = add_extract_block(files, [2], 'ParticleData')
+        particle = add_glyph(particles, 'Sphere', 'SphereGlyph')
+        particle.SetScaleFactor = PARTICLE_SCALE_FACTOR
+        particle.ScaleMode = 'scalar'
+
+        display = GetDisplayProperties(particle)
+        display.ColorArrayName = 'colors'
+
+        range = particle.PointData[0].GetRange()
+        display.LookupTable = MakeBlueToRedLT(range[0], range[1])
 
 
-### DYNAMIC
+    if SHELLS:
+        # Spheres.
+        spheres = add_extract_block(files, [4], 'SphereData')
+        sphere = add_glyph(spheres, 'Sphere', 'SphereGlyph')
+        sphere.GlyphType.ThetaResolution = RESOLUTION
+        sphere.GlyphType.PhiResolution = RESOLUTION
+        sphere.SetScaleFactor = 1
+        sphere.ScaleMode = 'scalar'
 
-if PARTICLES:
-    # Particles.
-    particles = add_extract_block(files, [2], 'ParticleData')
-    particle = add_glyph(particles, 'Sphere', 'SphereGlyph')
-    particle.SetScaleFactor = PARTICLE_SCALE_FACTOR
-    particle.ScaleMode = 'scalar'
+        display = GetDisplayProperties(sphere)
+        display.ColorArrayName = 'colors'
+        display.Representation = 'Wireframe'
+        display.Opacity = 0.5
 
-    display = GetDisplayProperties(particle)
-    display.ColorArrayName = 'colors'
-
-    range = particle.PointData[0].GetRange()
-    display.LookupTable = MakeBlueToRedLT(range[0], range[1])
-
-
-if SHELLS:
-    # Spheres.
-    spheres = add_extract_block(files, [4], 'SphereData')
-    sphere = add_glyph(spheres, 'Sphere', 'SphereGlyph')
-    sphere.GlyphType.ThetaResolution = RESOLUTION
-    sphere.GlyphType.PhiResolution = RESOLUTION
-    sphere.SetScaleFactor = 1
-    sphere.ScaleMode = 'scalar'
-
-    display = GetDisplayProperties(sphere)
-    display.ColorArrayName = 'colors'
-    display.Representation = 'Wireframe'
-    display.Opacity = 0.5
-
-    range = sphere.PointData[0].GetRange()
-    lookup_table = MakeBlueToRedLT(range[0], range[1])
-    display.LookupTable = lookup_table
+        range = sphere.PointData[0].GetRange()
+        lookup_table = MakeBlueToRedLT(range[0], range[1])
+        display.LookupTable = lookup_table
 
 
-    # Cylinders.
-    cylinders = add_extract_block(files, [6], 'CylinderData')
-    cylinder = add_tensor_glyph(cylinders, 'Cylinder', 'CylinderTensorGlyph')
-    cylinder.GlyphType.Resolution = RESOLUTION
+        # Cylinders.
+        cylinders = add_extract_block(files, [6], 'CylinderData')
+        cylinder = add_tensor_glyph(cylinders, 'Cylinder', 'CylinderTensorGlyph')
+        cylinder.GlyphType.Resolution = RESOLUTION
+
+        display = GetDisplayProperties(cylinder)
+        display.Representation = 'Wireframe'
+        display.Opacity = 1.0
+
+        # Todo.
+        #range = cylinder.PointData[0].GetRange()
+        #display.LookupTable = MakeBlueToRedLT(range[0], range[1])
+
+
+
+
+    ### STATIC
+
+    # Helix.
+    if HELIX:
+        # Todo.
+        helix = ProgrammableSource()
+        rename(helix, 'Helix')
+        helix_file = open(paraview_scripts_directory + '/helix.py', 'r')
+        helix.Script = helix_file.read()
+
+
+    if SURFACES:
+        # Cylindrical surfaces.
+        cylindrical_surfaces = add_extract_block(static, [2], 'CylindricalSurfaceData')
+        cylindrical_surface = add_tensor_glyph(cylindrical_surfaces, 'Cylinder', 'CylinderTensorGlyph')
+        # Todo. 
+        #cylindrical_surface = TensorGlyphWithCustomSource(cylindrical_surfaces)
+        #... Input=helix
+        display = GetDisplayProperties(cylindrical_surface)
+        display.Opacity = 0.2
+
+
+        # Planar surfaces.
+        planar_surfaces = add_extract_block(static, [4], 'PlanarSurfaceData')
+        planar_surface = add_tensor_glyph(planar_surfaces, 'Box', 'BoxTensorGlyph')
+        display = GetDisplayProperties(planar_surface)
+        display.Representation = 'Wireframe'
+        display.Opacity = 0.2
+
+
+        # Cuboidal surfaces.
+        cuboidal_surfaces = add_extract_block(static, [6], 'CuboidalSurfaceData')
+        cuboidal_surface = add_tensor_glyph(cuboidal_surfaces, 'Box', 'BoxTensorGlyph')
+        display = GetDisplayProperties(cuboidal_surface)
+        display.Representation = 'Wireframe'
+        display.Opacity = 1.0
+
+
+    # Initialize.
+    # Todo. Turn camera.
+    #SetActiveSource(cylindrical_surface)
+
+    renderer = Render()
+    renderer.Background = [0,0,0] # Black.
+    ResetCamera()
 
     display = GetDisplayProperties(cylinder)
-    display.Representation = 'Wireframe'
-    display.Opacity = 1.0
 
-    # Todo.
-    #range = cylinder.PointData[0].GetRange()
-    #display.LookupTable = MakeBlueToRedLT(range[0], range[1])
-
-
-
-
-### STATIC
-
-# Helix.
-if HELIX:
-    # Todo.
-    helix = ProgrammableSource()
-    rename(helix, 'Helix')
-    helix_file = open(paraview_scripts_directory + '/helix.py', 'r')
-    helix.Script = helix_file.read()
-
-
-if SURFACES:
-    # Cylindrical surfaces.
-    cylindrical_surfaces = add_extract_block(static, [2], 'CylindricalSurfaceData')
-    cylindrical_surface = add_tensor_glyph(cylindrical_surfaces, 'Cylinder', 'CylinderTensorGlyph')
-    # Todo. 
-    #cylindrical_surface = TensorGlyphWithCustomSource(cylindrical_surfaces)
-    #... Input=helix
-    display = GetDisplayProperties(cylindrical_surface)
-    display.Opacity = 0.2
-
-
-    # Planar surfaces.
-    planar_surfaces = add_extract_block(static, [4], 'PlanarSurfaceData')
-    planar_surface = add_tensor_glyph(planar_surfaces, 'Box', 'BoxTensorGlyph')
-    display = GetDisplayProperties(planar_surface)
-    display.Representation = 'Wireframe'
-    display.Opacity = 0.2
-
-
-    # Cuboidal surfaces.
-    cuboidal_surfaces = add_extract_block(static, [6], 'CuboidalSurfaceData')
-    cuboidal_surface = add_tensor_glyph(cuboidal_surfaces, 'Box', 'BoxTensorGlyph')
-    display = GetDisplayProperties(cuboidal_surface)
-    display.Representation = 'Wireframe'
-    display.Opacity = 1.0
-
-
-# Initialize.
-# Todo. Turn camera.
-#SetActiveSource(cylindrical_surface)
-
-renderer = Render()
-renderer.Background = [0,0,0] # Black.
-ResetCamera()
-
-display = GetDisplayProperties(cylinder)
+pipeline()
 
