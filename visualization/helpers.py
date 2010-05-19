c2be2123a4584b099b26ec807387a8f45089d7e4
thomie <miedema@amolf.nl>
2010-05-19 15:49:23 +0200

    Change. Make it work under Paraview 3.4.

---
+++ visualization/helpers.py
@@ -0,0 +1,322 @@
+from paraview import servermanager
+
+def Connect(ds_host=None, ds_port=11111, rs_host=None, rs_port=11111):
+    """Creates a connection to a server. Example usage:
+    > Connect("amber") # Connect to a single server at default port
+    > Connect("amber", 12345) # Connect to a single server at port 12345
+    > Connect("amber", 11111, "vis_cluster", 11111) # connect to data server, render server pair
+    This also create a default render view."""
+    servermanager.ProxyManager().UnRegisterProxies()
+    active_objects.view = None
+    active_objects.source = None
+    import gc
+    gc.collect()
+    servermanager.Disconnect()
+    cid = servermanager.Connect(ds_host, ds_port, rs_host, rs_port)
+    servermanager.ProxyManager().RegisterProxy("timekeeper", "tk", servermanager.misc.TimeKeeper())
+
+    return cid
+
+
+# TODO: Change this to take the array name and number of components. Register 
+# the lt under the name ncomp.array_name
+def MakeBlueToRedLT(min, max):
+    # Define RGB points. These are tuples of 4 values. First one is
+    # the scalar values, the other 3 the RGB values. 
+    rgbPoints = [min, 0, 0, 1, max, 1, 0, 0]
+    return CreateLookupTable(RGBPoints=rgbPoints, ColorSpace="HSV")
+    
+
+def CreateLookupTable(**params):
+    """Create and return a lookup table.  Optionally, parameters can be given
+    to assign to the lookup table.
+    """
+    lt = servermanager.rendering.PVLookupTable()
+    servermanager.Register(lt)
+    SetProperties(lt, **params)
+    return lt
+
+
+def SetProperties(proxy=None, **params):
+    """Sets one or more properties of the given pipeline object. If an argument
+    is not provided, the active source is used. Pass a list of property_name=value
+    pairs to this function to set property values. For example:
+     SetProperties(Center=[1, 2, 3], Radius=3.5)
+    """
+    if not proxy:
+        proxy = active_objects.source
+    for param in params.keys():
+        if not hasattr(proxy, param):
+            raise AttributeError("object has no property %s" % param)
+        setattr(proxy, param, params[param])
+
+
+def GetSources():
+    """Given the name of a source, return its Python object."""
+    return servermanager.ProxyManager().GetProxiesInGroup("sources")
+
+
+def Delete(proxy, version=6):
+    """Deletes the given pipeline object or the active source if no argument
+    is specified."""
+    if not proxy:
+        proxy = active_objects.source
+    # Unregister any helper proxies stored by a vtkSMProxyListDomain
+    for prop in proxy:
+        listdomain = prop.GetDomain('proxy_list')
+        if listdomain:
+            if listdomain.GetClassName() != 'vtkSMProxyListDomain':
+                continue
+            group = "pq_helper_proxies." + proxy.GetSelfIDAsString()
+            for i in xrange(listdomain.GetNumberOfProxies()):
+                pm = servermanager.ProxyManager()
+                iproxy = listdomain.GetProxy(i)
+                name = pm.GetProxyName(group, iproxy)
+                if iproxy and name:
+                    pm.UnRegisterProxy(group, name, iproxy)
+                    
+
+    # Remove source/view from time keeper
+    if version == 4 or version == 5:
+        # Paraview 3.4.
+        # Paraview 3.6 mimicking 3.4.
+        # Todo.
+        pass
+    else:
+        # Paraview 3.6.
+        tk = servermanager.ProxyManager().GetProxiesInGroup("timekeeper").values()[0]
+
+        if isinstance(proxy, servermanager.SourceProxy):
+            try:
+                idx = tk.TimeSources.index(proxy)
+                del tk.TimeSources[idx]
+                pass
+            except ValueError:
+                pass
+        else:
+            try:
+                idx = tk.Views.index(proxy)
+                del tk.Views[idx]
+            except ValueError:
+                pass
+    
+    # If this is a representation, remove it from all views.
+    if proxy.SMProxy.IsA("vtkSMRepresentationProxy"):
+        for view in GetRenderViews():
+            view.Representations.remove(proxy)
+    # If this is a source, remove the representation iff it has no consumers
+    # Also change the active source if necessary
+    elif proxy.SMProxy.IsA("vtkSMSourceProxy"):
+        sources = servermanager.ProxyManager().GetProxiesInGroup("sources")
+        if version == 4 or version == 5:
+            # Paraview 3.4.
+            # Paraview 3.6 mimicking 3.4.
+            # Todo.
+            pass
+        else:
+            # Paraview 3.6.
+            for i in range(proxy.GetNumberOfConsumers()):
+                if proxy.GetConsumerProxy(i) in sources:
+                    raise RuntimeError("Source has consumers. It cannot be deleted " +
+                      "until all consumers are deleted.")
+            if proxy == GetActiveSource():
+                if hasattr(proxy, "Input") and proxy.Input:
+                    if isinstance(proxy.Input, servermanager.Proxy):
+                        SetActiveSource(proxy.Input)
+                    else:
+                        SetActiveSource(proxy.Input[0])
+                else: SetActiveSource(None)
+        for rep in GetRepresentations().values():
+            if rep.Input == proxy:
+                # Hack to avoid 'Connection sink not found in the pipeline 
+                # model' in Paraview 3.6.
+                #rep.Input = None
+                Delete(rep)
+    # Change the active view if necessary
+    elif proxy.SMProxy.IsA("vtkSMRenderViewProxy"):
+        if proxy == GetActiveView():
+            if len(GetRenderViews()) > 0:
+                SetActiveView(GetRenderViews()[0])
+            else:
+                SetActiveView(None)
+
+    if version == 4 or version == 5:
+        # Paraview 3.4.
+        # Paraview 3.6 mimicking 3.4.
+        pass
+    else:
+        # Paraview 3.6.
+        if hasattr(proxy, "Input"):
+            proxy.Input = None
+    servermanager.UnRegister(proxy)
+
+
+def GetRepresentations():
+    """Returns all representations (display properties)."""
+    return servermanager.ProxyManager().GetProxiesInGroup("representations")
+
+
+def GetActiveSource():
+    "Returns the active source."
+    return active_objects.source
+
+
+def SetActiveSource(source):
+    "Sets the active source."
+    active_objects.source = source
+    
+
+def CreateRenderView():
+    "Creates and returns a 3D render view."
+    view = servermanager.CreateRenderView()
+    servermanager.ProxyManager().RegisterProxy("views", \
+      "my_view%d" % _funcs_internals.view_counter, view)
+    active_objects.view = view
+    _funcs_internals.view_counter += 1
+    
+    tk = servermanager.ProxyManager().GetProxiesInGroup("timekeeper").values()[0]
+    views = tk.Views
+    if not view in views:
+        views.append(view)
+
+    return view
+
+
+class _funcs_internals:
+    "Internal class."
+    first_render = True
+    view_counter = 0
+    rep_counter = 0
+
+
+def GetRenderViews():
+    "Returns all render views as a list."
+    return servermanager.GetRenderViews()
+
+
+def Show(proxy=None, view=None, **params):
+    """Turns the visibility of a given pipeline object on in the given view.
+    If pipeline object and/or view are not specified, active objects are used."""
+    if proxy == None:
+        proxy = GetActiveSource()
+    if proxy == None:
+        raise RuntimeError, "Show() needs a proxy argument or that an active source is set."
+    if not view and len(GetRenderViews()) == 0:
+        CreateRenderView()
+    rep = GetDisplayProperties(proxy, view)
+    if rep == None:
+        raise RuntimeError, "Could not create a representation object for proxy %s" % proxy.GetXMLLabel()
+    for param in params.keys():
+        setattr(rep, param, params[param])
+    rep.Visibility = 1
+    return rep
+
+class ActiveObjects(object):
+    """This class manages the active objects (source and view). The active
+    objects are shared between Python and the user interface. This class
+    is for internal use. Use the Set/Get methods for setting and getting
+    active objects."""
+    def __get_selection_model(self, name):
+        "Internal method."
+        pxm = servermanager.ProxyManager()
+        model = pxm.GetSelectionModel(name)
+        if not model:
+            model = servermanager.vtkSMProxySelectionModel()
+            pxm.RegisterSelectionModel(name, model)
+        return model
+
+    def set_view(self, view):
+        "Sets the active view."
+        active_view_model = self.__get_selection_model("ActiveView") 
+        if view:
+            active_view_model.SetCurrentProxy(view.SMProxy, 0)
+        else:
+            active_view_model.SetCurrentProxy(None, 0)
+
+    def get_view(self):
+        "Returns the active view."
+        return servermanager._getPyProxy(
+            self.__get_selection_model("ActiveView").GetCurrentProxy())
+
+    def set_source(self, source):
+        "Sets the active source."
+        active_sources_model = self.__get_selection_model("ActiveSources") 
+        if source:
+            # 3 == CLEAR_AND_SELECT
+            active_sources_model.SetCurrentProxy(source.SMProxy, 3)
+        else:
+            active_sources_model.SetCurrentProxy(None, 3)
+
+    def __convert_proxy(self, px):
+        "Internal method."
+        if not px:
+            return None
+        if px.IsA("vtkSMSourceProxy"):
+            return servermanager._getPyProxy(px)
+        else:
+            return servermanager.OutputPort(
+              servermanager._getPyProxy(px.GetSourceProxy()),
+              px.GetPortIndex())
+        
+    def get_source(self):
+        "Returns the active source."
+        return self.__convert_proxy(
+          self.__get_selection_model("ActiveSources").GetCurrentProxy())
+
+    def get_selected_sources(self):
+        "Returns the set of sources selected in the pipeline browser."
+        model = self.__get_selection_model("ActiveSources")
+        proxies = []
+        for i in xrange(model.GetNumberOfSelectedProxies()):
+            proxies.append(self.__convert_proxy(model.GetSelectedProxy(i)))
+        return proxies
+
+    view = property(get_view, set_view)
+    source = property(get_source, set_source)
+
+active_objects = ActiveObjects()
+
+
+def GetDisplayProperties(proxy=None, view=None):
+    """"Given a pipeline object and view, returns the corresponding representation object.
+    If pipeline object and/or view are not specified, active objects are used."""
+    return GetRepresentation(proxy, view)
+    
+
+def GetRepresentation(proxy=None, view=None):
+    """"Given a pipeline object and view, returns the corresponding representation object.
+    If pipeline object and view are not specified, active objects are used."""
+    if not view:
+        view = active_objects.view
+    if not proxy:
+        proxy = active_objects.source
+    rep = servermanager.GetRepresentation(proxy, view)
+    if not rep:
+        rep = servermanager.CreateRepresentation(proxy, view)
+        servermanager.ProxyManager().RegisterProxy("representations", \
+          "my_representation%d" % _funcs_internals.rep_counter, rep)
+        _funcs_internals.rep_counter += 1
+    return rep
+    
+
+def Render(view=None):
+    """Renders the given view (default value is active view)"""
+    if not view:
+        view = active_objects.view
+    view.StillRender()
+    if _funcs_internals.first_render:
+        view.ResetCamera()
+        view.StillRender()
+        _funcs_internals.first_render = False
+    return view
+        
+
+def ResetCamera(view=None):
+    """Resets the settings of the camera to preserver orientation but include
+    the whole scene. If an argument is not provided, the active view is
+    used."""
+    if not view:
+        view = active_objects.view
+    view.ResetCamera()
+    Render(view)
+
--- visualization/pipeline.py
+++ visualization/pipeline.py
@@ -1,16 +1,42 @@
+READERS   = False
+PARTICLES = False
+SHELLS    = False
+HELIX     = False
+SURFACES  = False
+
 # Settings
-
-READERS = True
+READERS   = True
 PARTICLES = True
-SHELLS = True
-HELIX = True
-SURFACES = True
+#SHELLS    = True
+#HELIX     = True
+#SURFACES  = True
 
-PARTICLE_SCALE_FACTOR = 1
+PARTICLE_SCALE_FACTOR = 4
 RESOLUTION = 18
 
-from paraview.simple import *  # Should be listed first.
-#from paraview.servermanager import *
+import sys
+sys.path.append(paraview_scripts_directory)
+import helpers
+
+import paraview
+
+from paraview import servermanager
+
+print 'version =', paraview.compatibility.GetVersion()
+
+try:
+    paraview.compatibility
+    paraview.compatibility.minor = 4
+    if servermanager.ActiveConnection:
+        # 6: paraview 3.6.
+        # 5: as much as possible compatible with paraview 3.4.
+        version = 5
+    else:
+        # Hack. I don't remember why this is needed.
+        version = 4
+except:
+    version = 4
+
 
 def clear():
     def name(proxy):
@@ -23,74 +49,77 @@ def clear():
             return 1
         return cmp(x,y)
 
-    for proxy in sorted(GetSources().itervalues(), compare_glyphs_first):
+    for proxy in sorted(helpers.GetSources().itervalues(), 
+                        compare_glyphs_first):
+        helpers.Delete(proxy, version) # Delete in simple.py needs hack in 3.6.
+
+
+def register(proxy, name):
+    if version == 4:
+        # Paraview 3.4.
+        name = None
+    else:
         # Hack to avoid 'Connection sink not found in the pipeline model' in 
+        # Paraview 3.6 mimicking 3.4.
         # Paraview 3.6.
-        if hasattr(proxy, "Input") and proxy.Input:
-            if isinstance(proxy.Input, servermanager.Proxy):
-                proxy.Input = None
-            else:
-                proxy.Input[0] = None
-
-        Delete(proxy)
-
-
-'''
-def __MakeBlueToRedLT(min, max):
-    # Define RGB points. These are tuples of 4 values. First one is
-    # the scalar values, the other 3 the RGB values. 
-    rgb_points = [min, 0, 0, 1, max, 1, 0, 0]
-    return CreateLookupTable(RGBPoints=rgb_points, ColorSpace="HSV")
-'''
-
-
-def rename(proxy, name):
-    # Hack to avoid 'Connection sink not found in the pipeline model' in 
-    # Paraview 3.6.
-    if hasattr(proxy, "Input") and proxy.Input:
-        if isinstance(proxy.Input, servermanager.Proxy):
+        if hasattr(proxy, "Input"):
             input = proxy.Input
         else:
-            input = proxy.Input[0]
-        proxy.Input = None
-    else:
-        input = None
+            input = None
 
-    # No need to unregister when using servermanager.sources/filters...(?)
-    servermanager.UnRegister(proxy)
-    servermanager.Register(proxy, registrationName=name)
+        # No need to unregister when using servermanager.sources/filters.
+        # Or: no need to register when using simple.Glyph() etc.
+        #servermanager.UnRegister(proxy)
+        servermanager.Register(proxy, registrationName=name)
 
-    # Reset input.
-    if input != None:
-        if isinstance(input, servermanager.Proxy):
+        # Reset input.
+        if input != None:
             proxy.Input = input
-        else:
-            proxy.Input[0] = input
 
 
 def addPVDReader(file, name):
-    reader = PVDReader(FileName=file)
-    rename(reader, name)
+    reader = servermanager.sources.PVDReader(FileName=file)
+    register(reader, name)
     return reader
 
-def add_glyph(data, type, name):
-    glyph = Glyph(data, GlyphType=type)
-    rename(glyph, name)
-    return glyph
-
 def add_extract_block(data, indices, name):
-    block = ExtractBlock(data, BlockIndices=indices)
-    rename(block, name)
-    block.UpdatePipeline(); # Otherwise SetScaleFactor doesn't work. Otherwise TensorGlyph error.
+    block = servermanager.filters.ExtractBlock(Input=data, 
+                                               BlockIndices=indices)
+    register(block, name)
+    # Otherwise SetScaleFactor doesn't work. Otherwise TensorGlyph error.
+    block.UpdatePipeline();
     return block
 
+
 def add_tensor_glyph(data, type, name):
-    tensor_glyph = TensorGlyph(data, GlyphType=type)
-    rename(tensor_glyph, name)
+    tensor_glyph = servermanager.filters.TensorGlyph(Input=data, 
+                                                     GlyphType=type)
+    #tensor_glyph = vtk.vtkTensorGlyph(data, GlyphType=type)
+    register(tensor_glyph, name)
     return tensor_glyph
 
+
 def pipeline():
-    clear()
+    if not servermanager.ActiveConnection:
+        if version == 4 or version == 5:
+            # Paraview 3.4.
+            # Paraview 3.6 mimicking 3.4.
+            servermanager.Connect()
+        else:
+            # Paraview 3.6.
+            helpers.Connect()
+
+    try:
+        views = servermanager.GetRenderViews()
+        if len(views) > 0:
+            print 'reuse render view'
+            renModule = views[0]
+        else:
+            print 'new render view'
+            renModule = servermanager.CreateRenderView()
+    except KeyError:
+        renModule = servermanager.CreateRenderView()
+
 
     if READERS:
         # Read dynamic data.
@@ -100,106 +129,182 @@ def pipeline():
         static = addPVDReader(simulation_data_directory + '/static.pvd', 'StaticDataReader')
 
 
-
-
-    ### DYNAMIC
-
     if PARTICLES:
         # Particles.
         particles = add_extract_block(files, [2], 'ParticleData')
-        particle = add_glyph(particles, 'Sphere', 'SphereGlyph')
-        particle.SetScaleFactor = PARTICLE_SCALE_FACTOR
-        particle.ScaleMode = 'scalar'
+        if version == 4:
+            # Paraview 3.4.
+            source = servermanager.sources.SphereSource()
+            particle = servermanager.filters.Glyph(Input=particles, 
+                                                   Source=source)
+            particle.SetScaleMode = 0
+        else:
+            # Paraview 3.6 mimicking 3.4.
+            # Paraview 3.6.
+            particle = servermanager.filters.Glyph(Input=particles, 
+                                                   GlyphType='Sphere')
+            particle.UpdatePipeline();
+            particle.ScaleMode = 'scalar'
 
-        display = GetDisplayProperties(particle)
+        register(particle, 'SphereGlyph')
+
+        particle.SetScaleFactor = PARTICLE_SCALE_FACTOR
+
+        if version == 4 or version == 5:
+            # Paraview 3.4.
+            # Paraview 3.6 mimicking 3.4.
+            display = servermanager.CreateRepresentation(particle, renModule)
+            display.SelectionVisibility = 1
+            particle
+        else:
+            # Paraview 3.6.
+            display = helpers.Show(particle)
+            #display = helpers.GetDisplayProperties(particle)
+
+        '''
         display.ColorArrayName = 'colors'
 
-        range = particle.PointData[0].GetRange()
-        display.LookupTable = MakeBlueToRedLT(range[0], range[1])
+        # Todo.
+        #range = particle.PointData[0].GetRange()
+        #display.LookupTable = helpers.MakeBlueToRedLT(range[0], range[1])
+        '''
 
 
     if SHELLS:
         # Spheres.
         spheres = add_extract_block(files, [4], 'SphereData')
-        sphere = add_glyph(spheres, 'Sphere', 'SphereGlyph')
-        sphere.GlyphType.ThetaResolution = RESOLUTION
-        sphere.GlyphType.PhiResolution = RESOLUTION
-        sphere.SetScaleFactor = 1
-        sphere.ScaleMode = 'scalar'
+        if version == 4:
+            # Paraview 3.4.
+            source = servermanager.sources.SphereSource()
+            sphere = servermanager.filters.Glyph(Input=spheres, Source=source)
+            sphere.SetScaleMode = 0
+            sphere.ThetaResolution = RESOLUTION
+            sphere.PhiResolution = RESOLUTION
+        else:
+            # Paraview 3.6 mimicking 3.4.
+            # Paraview 3.6.
+            sphere = servermanager.filters.Glyph(Input=spheres, 
+                                                 GlyphType='Sphere')
+            sphere.ScaleMode = 'scalar'
+            sphere.GlyphType.ThetaResolution = RESOLUTION
+            sphere.GlyphType.PhiResolution = RESOLUTION
 
-        display = GetDisplayProperties(sphere)
+        register(sphere, 'SphereGlyph')
+
+        sphere.SetScaleFactor = 1
+
+        if version == 4 or version == 5:
+            # Paraview 3.4.
+            # Paraview 3.6 mimicking 3.4.
+            display = servermanager.CreateRepresentation(sphere, renModule)
+            display.SelectionVisibility = 1
+        else:
+            # Paraview 3.6.
+            display = helpers.Show(sphere)
+            #display = helpers.GetDisplayProperties(sphere)
+
         display.ColorArrayName = 'colors'
         display.Representation = 'Wireframe'
         display.Opacity = 0.5
 
-        range = sphere.PointData[0].GetRange()
-        lookup_table = MakeBlueToRedLT(range[0], range[1])
-        display.LookupTable = lookup_table
+        # Todo.
+        #range = sphere.PointData[0].GetRange()
+        #lookup_table = helpers.MakeBlueToRedLT(range[0], range[1])
+        #display.LookupTable = lookup_table
 
-
+        ''' Todo.
         # Cylinders.
         cylinders = add_extract_block(files, [6], 'CylinderData')
-        cylinder = add_tensor_glyph(cylinders, 'Cylinder', 'CylinderTensorGlyph')
-        cylinder.GlyphType.Resolution = RESOLUTION
+        cylinder = add_tensor_glyph(cylinders, 'Cylinder', 
+                                    'CylinderTensorGlyph')
+        #helpers.Show(cylinder)
+        # Todo.
+        #cylinder.GlyphType.Resolution = RESOLUTION
 
-        display = GetDisplayProperties(cylinder)
+        display = helpers.GetDisplayProperties(cylinder)
         display.Representation = 'Wireframe'
         display.Opacity = 1.0
 
         # Todo.
         #range = cylinder.PointData[0].GetRange()
-        #display.LookupTable = MakeBlueToRedLT(range[0], range[1])
+        #display.LookupTable = helpers.MakeBlueToRedLT(range[0], range[1])
+        '''
 
 
-
-
-    ### STATIC
-
-    # Helix.
     if HELIX:
-        # Todo.
-        helix = ProgrammableSource()
-        rename(helix, 'Helix')
         helix_file = open(paraview_scripts_directory + '/helix.py', 'r')
-        helix.Script = helix_file.read()
+        if version == 4 or version == 5:
+            # Paraview 3.4.
+            # Paraview 3.6 mimicking 3.4.
+            #helix = ProgrammableFilter()
+            #helix.Script = helix_file.read()
+            #servermanager.Register(helix) # Needed, or register.
+            pass
+        else:
+            # Paraview 3.6.
+            # Todo. Scale it.
+            helix = servermanager.sources.ProgrammableSource()
+            helix.Script = helix_file.read()
+            register(helix, 'Helix')
+            helpers.Show(helix)
 
 
-    if SURFACES:
+    if SURFACES and version == 6:
         # Cylindrical surfaces.
-        cylindrical_surfaces = add_extract_block(static, [2], 'CylindricalSurfaceData')
-        cylindrical_surface = add_tensor_glyph(cylindrical_surfaces, 'Cylinder', 'CylinderTensorGlyph')
+        cylindrical_surfaces = add_extract_block(static, [2], 
+                                                 'CylindricalSurfaceData')
+        cylindrical_surface = add_tensor_glyph(cylindrical_surfaces, 
+                                               'Cylinder', 
+                                               'CylinderTensorGlyph')
+        #helpers.Show(cylindrical_surface)
         # Todo. 
         #cylindrical_surface = TensorGlyphWithCustomSource(cylindrical_surfaces)
         #... Input=helix
-        display = GetDisplayProperties(cylindrical_surface)
+        display = helpers.GetDisplayProperties(cylindrical_surface)
         display.Opacity = 0.2
 
 
         # Planar surfaces.
         planar_surfaces = add_extract_block(static, [4], 'PlanarSurfaceData')
-        planar_surface = add_tensor_glyph(planar_surfaces, 'Box', 'BoxTensorGlyph')
-        display = GetDisplayProperties(planar_surface)
+        planar_surface = add_tensor_glyph(planar_surfaces, 'Box', 
+                                          'BoxTensorGlyph')
+        #helpers.Show(planar_surface)
+        display = helpers.GetDisplayProperties(planar_surface)
         display.Representation = 'Wireframe'
         display.Opacity = 0.2
 
 
         # Cuboidal surfaces.
-        cuboidal_surfaces = add_extract_block(static, [6], 'CuboidalSurfaceData')
-        cuboidal_surface = add_tensor_glyph(cuboidal_surfaces, 'Box', 'BoxTensorGlyph')
-        display = GetDisplayProperties(cuboidal_surface)
+        cuboidal_surfaces = add_extract_block(static, [6], 
+                                              'CuboidalSurfaceData')
+        cuboidal_surface = add_tensor_glyph(cuboidal_surfaces, 'Box', 
+                                            'BoxTensorGlyph')
+        #helpers.Show(cuboidal_surface)
+        display = helpers.GetDisplayProperties(cuboidal_surface)
         display.Representation = 'Wireframe'
         display.Opacity = 1.0
 
 
-    # Initialize.
     # Todo. Turn camera.
     #SetActiveSource(cylindrical_surface)
 
-    renderer = Render()
-    renderer.Background = [0,0,0] # Black.
-    ResetCamera()
+    # Todo.
+    if version == 4 or version == 5:
+        # Paraview 3.4.
+        # Paraview 3.6 mimicking 3.4.
+        renModule.StillRender()
+    else:
+        # Paraview 3.6.
+        renderer = helpers.Render()
+        renderer.Background = [0,0,0] # Black.
+        helpers.ResetCamera()
 
-    display = GetDisplayProperties(cylinder)
+    #display = helpers.GetDisplayProperties(cylinder)
 
+
+print 'clear pipeline'
+clear()
+print 'build pipeline'
 pipeline()
+
 
